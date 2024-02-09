8673bca881af3faaa61715d533dd9c6721590de4
RAFAEL <rafaellichen@gmail.com>
2024-02-09 11:35:51 -0800

    Delete web/auth directory

--- web/auth/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    distDir: "../../labeler_client/web/auth/out",
-};
-module.exports = nextConfig;
